acf2380f0c21d00b403b3a6e25d3c0665aa9e7fc
luis-henrique-carvalho <luisdev9225@gmail.com>
2026-04-15 10:00:23 -0300

    feat: remove unused migration files and update Prisma client initialization

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,7 +4,14 @@ import { PrismaClient } from "../../generated/prisma/client";
 
 const connectionString = `${process.env.DATABASE_URL}`;
 
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient;
+};
+
 const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
+
+const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export { prisma };
