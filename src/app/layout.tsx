aff8a40221ae6856a8defa2b6abfc45644ed52a5
luis-henrique-carvalho <luisdev9225@gmail.com>
2026-04-15 09:06:44 -0300

    chore: remove unused IBM_Plex_Sans font import from layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, IBM_Plex_Sans } from "next/font/google";
+import { Geist, Geist_Mono } from "next/font/google";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import "./globals.css";
 import { cn } from "@/lib/utils";
